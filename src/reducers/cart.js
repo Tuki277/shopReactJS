994a490fdca84f45ac12d0f9863aaa745a85622c
Tran Hoang Huy <huybeos2707@gmail.com>
2021-02-27 08:58:33 +0700

    fix

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -5,11 +5,11 @@ var initialState = data ? data : []
 
 var findProductInCart = (cart, product) => {
     console.log('func find product in reducer ======= ', product[0])
-    console.log('func find product in cart ============= ', cart[0].product)
+    console.log('func find product in cart ============= ', cart[0])
     var index = -1 // = -1 là không tìm thấy
     if(cart.length > 0) {
         for (var i = 0; i < cart.length; i++) {
-            if (cart[i].product[0].id === product[0].id) {
+            if (cart[i].products[0].product_id === product[0].product_id) {
                 index = i
                 break
             }
